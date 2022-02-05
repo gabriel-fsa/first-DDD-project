986f94f577bca1bd7d6ef1884ef7a1b6ffd6f53b
Gabriel <gabriel_fsa@live.com>
2022-02-05 01:44:17 -0300

    refactor: ensure the SignUpController.handle implements interfaces and remove the duplicated code

--- src/presentation/controller/signup.ts
+++ src/presentation/controller/signup.ts
@@ -1,8 +1,20 @@
+import { HttpRequest, HttpResponse } from '../protocols/http'
+
 export class SignUpController {
-  async handle(request:any) {
-    return {
-      statusCode: 400,
-      body: new Error('Missing param: name'),
+  async handle(request:HttpRequest): Promise<HttpResponse> {
+    const requiredParams = ['name', 'email']
+    // eslint-disable-next-line no-restricted-syntax
+    for (const param of requiredParams) {
+      if (!request.body[param]) {
+        return Promise.resolve({
+          statusCode: 400,
+          body: new Error(`Missing param: ${param}`),
+        })
+      }
     }
+    return Promise.resolve({
+      statusCode: 200,
+      body: 'No errors',
+    })
   }
 }
